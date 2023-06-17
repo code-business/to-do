ea7601a63e3c512de73c9b79f1c7cabc557b00c3
Riddhesh Ganatra <riddheshganatra@gmail.com>
2023-06-17 11:41:11 +0530

    simple routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" Component={Login} />{" "}
+        <Route exact path="/register" Component={Register} />{" "}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
