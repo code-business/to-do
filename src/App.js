140082d6ca1f07497c57849b75d94bb750e3b599
Riddhesh Ganatra <riddheshganatra@gmail.com>
2023-06-17 12:03:07 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/login" Component={Login} />{" "}
+        <Route exact path="/" Component={Login} />{" "}
         <Route exact path="/register" Component={Register} />{" "}
       </Routes>
     </BrowserRouter>
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,88 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Formik } from "formik";
 
 function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {};
+
   return (
     <>
       <h1>Login</h1>
+      <input
+        type={"email"}
+        placeholder="Enter your email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type={"password"}
+        placeholder="Enter your password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          login();
+        }}
+      >
+        Login
+      </button>
+
+      {/* <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik> */}
+
+      <button
+        onClick={() => {
+          navigate("register");
+        }}
+      >
+        Go to register
+      </button>
     </>
   );
 }
